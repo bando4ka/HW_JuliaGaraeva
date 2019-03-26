3b3be7b90ee028f955411f111d125b67fe6b01cb
Bando4ka <bando4ka@mail.ru>
2019-03-27 01:06:38 +0200

    Added Part 3 - If Else

--- 2nd_HW.js
+++ 2nd_HW.js
@@ -85,8 +85,67 @@ null + 5
 /* null -> 0, 0 + 5 = 5 */
 
 //8
+
 true + undefined
 //NaN
 /* В JS true + undefined дает Not a Number, так как выражение идентично 1 + NaN, что даст NaN. */
 
 
+//Part 3 - If else
+
+//1
+var a = "hidden";
+
+if(a = "hidden"){
+  console.log(a = "visible");
+} else {
+  console.log(a = "hidden");
+};
+//"visible"
+
+//2
+var a = 10;
+
+if(a === 0){
+  console.log(a === 1);
+} else if(a < 0){
+  console.log(a = "less then zero");
+}else if(a > 0){
+  console.log(a *=10);
+};
+//100
+
+//3
+
+let car = { name: 'Lexus', age: 10, create: 2008, needRepair: false }
+if(car.age > 5) {
+  console.log("Need Repair");
+  car.needRepair = true;
+ }else{
+  car.needRepair = false;
+};
+console.log(car);
+
+//4
+ let item = { name: 'Intel core i7', price: '100$', discount: '15%' }
+ if(item.discount !== undefined ) {
+   item.priceWithDiscount = parseInt(item.price) * (100 - parseInt(item.discount))/100;
+   console.log(`Your price is ${item.priceWithDiscount}US$`);
+ } else{
+   console.log(`Your price is ${item.price}US$`);
+ };
+
+//5
+let product = {
+name: "Яблоко",
+price: "25$"
+}
+  let min = 10;
+  let max = 20;
+
+if(((parseInt(product.price)) >= min) && ((parseInt(product.price)) <= max)){
+
+  console.log(product.name);
+} else {
+  console.log("Товаров не найдено");
+};
