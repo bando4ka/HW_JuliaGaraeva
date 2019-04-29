f072bb17c00e28dfe824ec477b1a54f42a14b4f6
Bando4ka <bando4ka@mail.ru>
2019-04-29 18:51:49 +0300

    Changed

--- 6th_HW/js/jSForPart2.js
+++ 6th_HW/js/jSForPart2.js
@@ -7,6 +7,7 @@ isParent(document.body.children[0], document.querySelector('mark'));
 // true так как первый див является родительским элементом для mark
 isParent(document.querySelector('ul'), document.querySelector('mark'));
 // false так ul НЕ является родительским элементом для mark*/
+console.log('---------- 1----------');
 function isParent(parent, child) {
     //parent.contains(child) ? true : false
     if (parent.contains(child)) {
@@ -17,10 +18,32 @@ function isParent(parent, child) {
 }
 console.log(isParent(document.body.children[0], document.querySelector('mark')));
 console.log(isParent(document.querySelector('ul'), document.querySelector('mark')));
+
+//2. Получить список всех ссылок, которые не находятся внутри списка ul
+console.log('---------- 2----------');
+let allLinksWithoutUl = document.links;
+for(let link of allLinksWithoutUl) {
+    if(!link.closest('ul')) {
+        console.log(link);
+    }
+}
+
+//3. Найти элемент, который находится перед и после списка ul
+console.log('---------- 3----------');
+let element = document.querySelector('ul');
+let prevElement = element.previousElementSibling;
+let nextElement = element.nextElementSibling;
+console.log(prevElement);
+console.log(nextElement);
+
+
+
+//4. Посчитать количество элементов li в списке
+console.log('---------- 4 ----------');
+let numberOfLi = document.querySelectorAll('ul li').length;
+console.log(numberOfLi);
+
 /*
-2. Получить список всех ссылок, которые не находятся внутри списка ul
-3. Найти элемент, который находится перед и после списка ul
-4. Посчитать количество элементов li в списке
 5. В коде с занятия написать функцию по редактированию задачи.
 6. Подумать и улучшить функцию generateId();
  */
